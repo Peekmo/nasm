12eaafb5b2a0c6f4ec7eb4153c119fde8012d92a
Peekmo <axel.anceau@gmail.com>
2013-11-02 08:02:53 +0100

    Adding print_hex procedure

--- bootsect.asm
+++ bootsect.asm
@@ -1,105 +1,25 @@
-[BITS 16]  ; indique a nasm que l'on travaille en 16 bits
-[ORG 0x0]
+[BITS 16]      ; 16 bit code generation
+[ORG 0x7C00]   ; Origin location
 
-; initialisation des segments en 0x07C00
-    mov ax, 0x07C0
-    mov ds, ax
-    mov es, ax
-    mov ax, 0x8000
-    mov ss, ax
-    mov sp, 0xf000    ; stack de 0x8F000 -> 0x80000
+mov bx, HELLO_MSG
+call print
 
-	call start
+mov bx, GOODBYE_MSG
+call print
 
-end:
-	jmp end
+mov dx, 0x1fb6
+call print_hex
 
-;--- Variables ---
-	os1: db "================================", 13
-	os2: db "============ MoonOS ============", 13
-	os3: db "================================", 13, 13
-    init: db "Initialization", 13
-    loading: db "Loading....", 13
-    login: db "Login", 13, 0
-;-----------------
+jmp $
 
-start:
-   	mov si, os1
-	call show
-	call input
+%include "functions.asm"
 
-input:
-	mov ah, 0x02
-    mov cx, 1
-.wait:
-	mov ah, 0x01
-	int 0x16
-	jz .wait
-	
-	mov ah, 0x00
-	int 0x16
-	mov [si], al
-	inc si
-	cmp al, 13
-	je .endWait	
-	
-	mov ah, 0x0A
-	int 0x10
-	inc dl
-	mov ah, 0x02
-	int 0x10
-	jmp .wait
+; Data
+HEX_OUT: db "0x000", 0
+HEX_LEN equ $-HEX_OUT
+HELLO_MSG: db "Hello World !", 13, 10, 0
+GOODBYE_MSG: db "Goodbye World !", 13, 10, 0
 
-.endWait:
-	inc dh
-	xor dl, dl
-	mov ah, 0x02
-	int 0x10
-	mov byte [si], 0
-	call show
-	ret
-
-show:
-	push ax
-	push bx
-	push cx
-	push dx
-
-	mov ah, 0x03
-	int 0x10
-	mov cx, 1
-.debut:
-    mov al, [si] 
-    cmp al, 0     ; fin chaine ?
-    jz .fin
-
-	cmp al, 13    ; retour à la ligne ?
-    je .newLine
-
-.line:
-    mov ah, 0x02  
-    int 0x10
-    mov ah, 0x0A
-    int 0x10
-	inc si
-	inc dl
-    jmp .debut
-
-.fin:
-	pop dx
-	pop cx
-	pop bx
-	pop ax
-
-	inc dh
-    ret
-
-.newLine:
-    inc dh
-	inc si
-	xor dl, dl
-	jmp .debut
-
-;--- NOP jusqu'a 510 ---
-    times 510-($-$$) db 144
-    dw 0xAA55
+; End Matter
+times 510-($-$$) db 0	; Fill the rest with zeros
+dw 0xAA55		; Boot loader signature
--- functions.asm
+++ functions.asm
@@ -1,14 +1,3 @@
-[BITS 16]      ; 16 bit code generation
-[ORG 0x7C00]   ; Origin location
-
-mov bx, HELLO_MSG
-call print
-
-mov bx, GOODBYE_MSG
-call print
-
-jmp $
-
 print:
 	pusha
 	mov si, bx 
@@ -27,10 +16,50 @@ print:
 	popa
 	ret
 
-; Data
-HELLO_MSG: db "Hello World !", 13, 10, 0
-GOODBYE_MSG: db "Goodbye World !", 13, 10, 0
+print_hex:
+	pusha	
+	mov ebx, 0         ; Increment
+	mov si, HEX_OUT	   ; Met la chaine dans si
+	add si, HEX_LEN    ; Place le pointeur à la fin de la chaine
+	mov byte [si], 0   ; Rajoute le caractère null à la fin de la chaine
+	
+.nexthex:
+	mov cx, dx         ; Enregistre la valeur hexa dans une variable
+	add ebx, 1         ; Incremente + 1
+	cmp ebx, 4         ; Tant que pas plus grand que 4
+	jg .endhex
+	
+	and cl, 0x000F     ; Masque en ET sur les 4 derniers bit
+	cmp al,70		   ; Probleme quand F est lettre précédente on a un chiffre en plus
+	je .deccl
 
-; End Matter
-times 510-($-$$) db 0	; Fill the rest with zeros
-dw 0xAA55		; Boot loader signature
+	.backdec:
+	cmp cl,10          ; Si plus petit que 10 -> offset de 48 sinon 55
+	jl .offsetNumber
+	jmp .offsetLetter	
+
+	.nextOpe:
+	dec si              ; On recule le pointeur d'un octet
+	mov [si], cl        ; On met la valeur du caractère à cette position
+	shr dx, 4           ; Décalage de bit de 4 vers la droite
+	jmp .nexthex        
+
+.deccl:
+	dec cl
+	jmp .backdec
+
+.offsetNumber:
+	add cl,48
+	jmp .nextOpe
+
+.offsetLetter:
+	add cl,55
+	jmp .nextOpe
+
+.endhex:
+	sub si, 2           ; Retour du pointeur au début de la chaine (Ox)
+	mov bx, si			; Assigne la valeur de la chaine à bx pour print
+	call print
+	popa
+	ret
+
